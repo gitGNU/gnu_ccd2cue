cc64a6e575f08464f9b3ccfa4d19f14f99d80f01
Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
2013-11-08 07:02:53 +0000

    Modified license notice in order to cite GNU.

--- src/i18n.h
+++ src/i18n.h
@@ -1,21 +1,22 @@
-/* i18n.h -- Internationalization support
+/*
+ i18n.h -- Internationalization support
 
-   Copyright 2013 Bruno Félix Rezende Ribeiro <oitofelix@gnu.org>
+ Copyright (C) 2010, 2013 Bruno Félix Rezende Ribeiro
+ <oitofelix@gnu.org>
 
-   This file is part of ccd2cue.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3, or (at your option)
+ any later version.
 
-   ccd2cue is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   ccd2cue is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 #ifndef CCD2CUE_I18N_H
